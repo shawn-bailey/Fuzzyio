4a409b4cce9f18815dddd4beecccaa254d21536e
shawn-bailey <shawn.bailey@textnow.com>
2025-04-30 20:17:56 -0400

    centre view

--- app/src/main/java/com/shnoozie/fuzzyio/components/MainScreen.kt
+++ app/src/main/java/com/shnoozie/fuzzyio/components/MainScreen.kt
@@ -1,11 +1,14 @@
 package com.shnoozie.fuzzyio.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -27,7 +30,11 @@ fun NoiseButton(
     toggleState: () -> Unit = {},
     modifier: Modifier = Modifier
 ) {
-    Column {
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text(
             text = "White Noise Generator",
             modifier = modifier
